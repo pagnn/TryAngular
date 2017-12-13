5867c094a01989e5e81ad86115fab0828a1aed36
Sylvia <sylviawei19950920@gmail.com>
2017-12-13 10:23:03 +0800

    Ready for heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,OnDestroy  } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -37,4 +37,4 @@ export class HomeComponent implements OnInit,OnDestroy{
   }
 
   }
-}
+
